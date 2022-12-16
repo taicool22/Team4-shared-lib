7b367ead54a54f45cf5e7994d2a3d585355c93dc
taicool22 <taiwo08@gmail.com>
2022-12-16 09:44:01 -0600

    librar code added
    Signed-off-by: taicool22 <taiwo08@gmail.com>

---
+++ vars/etechApp.groovy
@@ -0,0 +1,27 @@
+def call(string repoUrl){
+    pipeline {
+       agent any
+       tools {
+           maven 'maven'
+       }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh 'mvn --version'
+                   sh 'java -version'
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                          url: "${repoUrl}"
+               }
+           }
+           stage("to-test-maven") {
+               steps {
+                   sh 'mvn -v'
+               }
+           }
+       }
+}
+}
